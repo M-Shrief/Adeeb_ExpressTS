477d00944a4ae73b3804654120a7bd6b5cf0662c
M-Shrief <m.shrief.dev@proton.me>
2023-08-01 11:38:14 +0300

    removed isLength validation from validationChain, and added the same constraints to mongoose model.

--- src/components/poet/poet.model.ts
+++ src/components/poet/poet.model.ts
@@ -8,14 +8,17 @@ const poetSchema = new Schema(
     name: {
       type: String,
       required: true,
+      length: {minlength: 4, maxlength: 50},
     },
     time_period: {
       type: String,
       required: true,
+      length: {minlength: 4, maxlength: 50},
     },
     bio: {
       type: String,
       required: true,
+      length: {minlength: 4, maxlength: 500},
     },
     reviewed: {
       type: Boolean,
