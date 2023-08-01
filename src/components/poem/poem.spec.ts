d8eb7117c108f2b2d765b32f7fef8a5f7774278b
M-Shrief <m.shrief.dev@proton.me>
2023-08-01 13:22:18 +0300

    decoupled tests from each other, and added error tests with GET /poem/:id

--- src/components/poem/poem.spec.ts
+++ src/components/poem/poem.spec.ts
@@ -4,9 +4,9 @@ import {describe, it} from 'mocha'
 import {baseHttp} from '../../utils/axios';
 import HttpStatusCode from '../../utils/httpStatusCode';
 // Types
-import { PoemType } from '../../interfaces/poem.interface';
+import { AxiosError } from 'axios';
+import { ERROR_MSG, PoemType } from '../../interfaces/poem.interface';
 
-let poemId: string;
 describe('GET /poems', async () => {
     it('Responds with the right JSON body', async () => {
         const req = await baseHttp.get('poems');
@@ -21,11 +21,15 @@ describe('GET /poems', async () => {
         assert.isArray(poems[0].verses);
 
 
-        poemId = poems[0]._id;
     })
 })
 
 describe('GET /poem/:id', async () => {
+    let poemId: string;
+    before(async () => {
+        const req = await baseHttp.get('poems');
+        poemId = req.data[0]._id;
+    })
     it('Responds with the right JSON body', async () => {
         const req = await baseHttp.get(`poem/${poemId}`);
     
@@ -37,5 +41,37 @@ describe('GET /poem/:id', async () => {
         assert.isDefined(poem.poet)
         assert.isDefined(poem.reviewed)
         assert.isArray(poem.verses);    
+    })
+    it('gets 404 with nonExisting MongoId', async () => {
+        try {
+            const corruptedId = poemId.replace(poemId[5], 'a');
+            await baseHttp.get(`poem/${corruptedId}`);
+        } catch(error) {
+            if(error instanceof AxiosError) {
+                assert.strictEqual(error.response!.status, HttpStatusCode.NOT_FOUND);
+        
+                assert.isString(error.response!.data.message)
+                assert.equal(error.response!.data.message, ERROR_MSG.NOT_FOUND)    
+                return;
+            }
+            throw error;
+        }
+
+    })
+
+    it('gets 400 with wrong :id format', async () => {
+        try {
+            await baseHttp.get(`poem/22`);
+        } catch(error) {
+            if(error instanceof AxiosError) {
+                assert.strictEqual(error.response!.status, HttpStatusCode.BAD_REQUEST);
+        
+                assert.isString(error.response!.data.message)
+                assert.equal(error.response!.data.message, ERROR_MSG.NOT_FOUND)    
+                return;
+            }
+            throw error;
+        }
+
     })
 })
